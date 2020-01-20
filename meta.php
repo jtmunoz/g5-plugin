2849d7d45042b054f2c2dd72f90c5abd20c257fd
jtmunoz <jordantmunoz@gmail.com>
2020-01-20 14:50:52 -0800

    adds deactivation method

--- meta.php
+++ meta.php
@@ -11,6 +11,16 @@
 
 // activate_plugin('meta/meta.php');
 
+/*
+// Deactivation Function
+function meta_on_deactivation() {
+	if ( !current_user_can('activate_plugins') ) return;
+	flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'meta_on_deactivation' );
+wp_die('Meta Plugin has been Deactivated');
+*/
+
 //Moves Yoast to Bottom of Admin UI
 function yoasttobottom() {
   return 'low';

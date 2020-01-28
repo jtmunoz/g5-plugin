40cdd8c47a14f5f0cd90f73615df11136f0a02e3
jtmunoz <jordantmunoz@gmail.com>
2020-01-27 16:37:55 -0800

    updates code

--- uninstall.php
+++ uninstall.php
@@ -1,21 +1,21 @@
 <?php 
 
-	if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-		exit;
-	}
+	// if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	// 	exit;
+	// }
 
-	if ( ! current_user_can( 'activate_plugins' ) ) {
-		return;
-	}
+	// if (  current_user_can( 'activate_plugins' ) ) {
+	// 	return;
+	// }
 
-	$plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
-	check_admin_referer( "deactivate-plugin_meta" );
+	// $plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+	// check_admin_referer( "deactivate-plugin_meta" );
 
-	if ( ! current_user_can( 'activate_plugins' ) ) {
-		return;
-	}
-	check_admin_referer( 'bulk-plugins' );
+	// if (  current_user_can( 'activate_plugins' ) ) {
+	// 	return;
+	// }
+	// check_admin_referer( 'bulk-plugins' );
 
-	if ( __FILE__ != WP_UNINSTALL_PLUGIN ) {
-		return;
-	}
+	// if ( __FILE__ != WP_UNINSTALL_PLUGIN ) {
+	// 	return;
+	// }

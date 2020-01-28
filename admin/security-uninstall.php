6b524dc8ed2adce79c4bf502d6b439ae401a1481
jtmunoz <jordantmunoz@gmail.com>
2020-01-27 16:08:52 -0800

    updates uninstall

--- admin/security-uninstall.php
+++ admin/security-uninstall.php
@@ -1,9 +1,9 @@
 <?php 
 
-	if ( ! is_super_admin() ) {
+	if ( ! current_user_can( 'activate_plugins' ) ) {
 		return;
 	}
-	
+
 	check_admin_referer( 'bulk-plugins' );
 
 	if ( __FILE__ != WP_UNINSTALL_PLUGIN ) {

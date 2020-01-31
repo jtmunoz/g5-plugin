7972e43060b0e02dce6b14b1f6ba740e53326cc8
jtmunoz <jordantmunoz@gmail.com>
2020-01-31 15:23:06 -0800

    updates workshop shortcode

--- meta.php
+++ meta.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Meta
  * Plugin URI: 
- * Description: A create custom post types.
+ * Description: A plugin to create custom post types.
  * Version: 1.0
  * Author: Jordan Muñoz
  * Author URI: 
